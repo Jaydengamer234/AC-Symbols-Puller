b68e546eb4880ddd9e4713b3370c5da8c32ac53a
Jaydengamer234 <jaydenhaan14@outlook.com>
2026-06-24 04:36:03 -0400

    Update Program.cs

--- ACPartialDownloader/Program.cs
+++ ACPartialDownloader/Program.cs
@@ -1,3 +1,16 @@
-using ACPartialDownloader;
+using System;
+using System.Threading.Tasks;
 
-await DepotPuller.RunAsync();
+namespace ACPartialDownloader
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string user = Environment.GetEnvironmentVariable("STEAM_USER");
+            string pass = Environment.GetEnvironmentVariable("STEAM_PASS");
+
+            await SteamKitDownloader.RunAsync(user, pass);
+        }
+    }
+}
